2f90ce2bcca305f1b9fe1e378c7e80bfdd529edd
Rudra Pratap Singh <144105327+Rudra78996@users.noreply.github.com>
2024-05-25 09:25:01 +0530

    url change

--- client/src/helper/socket.jsx
+++ client/src/helper/socket.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(()=>io("http://localhost:8080/"), []);
+  const socket = useMemo(()=>io("https://chathub-backend-2.onrender.com"), []);
   return (
     <socketContext.Provider value={{ socket }}>
       {props.children}
