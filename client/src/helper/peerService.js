7df780e412e5fb9c31e1b3d6d2462671813f1fcb
Rudra Pratap Singh <144105327+Rudra78996@users.noreply.github.com>
2024-05-25 11:36:16 +0530

    updated url

--- client/src/helper/peerService.js
+++ client/src/helper/peerService.js
@@ -1,42 +1,43 @@
-class PeerProvider{
-    constructor() {
-        if (!this.peer) {
-          this.peer = new RTCPeerConnection({
-            iceServers: [
-              {
-                urls: [
-                  "stun:stun.l.google.com:19302",
-                  "stun:global.stun.twilio.com:3478",
-                ],
-              },
-            ],
-          });
-        }
+class PeerProvider {
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          [
+            { urls: "stun:freeturn.net:5349" },
+            {
+              urls: "turns:freeturn.tel:5349",
+              username: "free",
+              credential: "free",
+            },
+          ],
+        ],
+      });
     }
+  }
 
-    async getAnswer(offer) {
-        if (this.peer) {
-          await this.peer.setRemoteDescription(offer);
-          const ans = await this.peer.createAnswer();
-          await this.peer.setLocalDescription(new RTCSessionDescription(ans));
-          return ans;
-        }
-    } 
-
-    async setRemoteDescription(ans) {
-        if (this.peer) {
-          await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
-        }
+  async getAnswer(offer) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(offer);
+      const ans = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+      return ans;
     }
+  }
 
-    async getOffer() {
-        if (this.peer) {
-          const offer = await this.peer.createOffer();
-          await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-          return offer;
-        }
+  async setRemoteDescription(ans) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
     }
+  }
 
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
+    }
+  }
 }
 
 export default new PeerProvider();
