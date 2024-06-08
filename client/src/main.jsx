a0692ce0d114a6e34ed6858db8bbe295d67edccf
Rudra Pratap Singh <144105327+Rudra78996@users.noreply.github.com>
2024-06-09 00:05:35 +0530

    vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
       <ToastContainer />
     </BrowserRouter>
